d2daaf46ea6b3d0862fd44418cabd64ec1d5746d
seongunKim <seongun1@github.com>
2025-07-08 01:05:06 -0700

    Removed build artifacts from repo

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yujin/smartfactory_ws/src
-# Build directory: /home/yujin/smartfactory_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("path_planning")
